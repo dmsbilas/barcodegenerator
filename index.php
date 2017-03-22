049a44058b8fdd126b3bccc577630d2c9a777bb2
dmsbilas <dmsbilas@yahoo.com>
2017-03-22 13:54:47 +0600

    adding form for mentioning amount of barcode images

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title> ABC Barcode Generator </title>
+    </head>
+    <body>
+        <form action="generate_barcode.php" method="post" >
+            <table>
+                <tr>
+                    <td>Total Barcodes:</td>
+                    <td><input type="text" name="barcode_amount" /></td>
+                </tr>
+                <tr>
+                    <td>&nbsp</td>
+                    <td><input type="submit" value="Generate Barcodes" /></td>
+                </tr>
+                
+
+            </table>
+            
+            
+        </form>
+    </body>
+</html>
